f6c2a6bcbbcc7729d8601d7ee091d13daeccc0ce
Lyquythuan3112 <adlqt312@gmail.com>
2023-06-20 21:00:26 +0700

    a

--- app.js
+++ app.js
@@ -1,31 +1,34 @@
+// app.js
+
 const express = require('express');
 const mongoose = require('mongoose');
-
+const bodyParser = require('body-parser');
 const app = express();
-const port = process.env.PORT || 3000;
 
-// MongoDB connection
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-  .then(() => console.log('Connected to MongoDB'))
-  .catch((error) => console.error('Failed to connect to MongoDB', error));
 
 // Middleware
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.set('views', 'views'); // Specify the directory for views
 
-// Routes
+var dbURI = "mongodb+srv://Andy:123456789aA@asmcloud.3jelcj0.mongodb.net/CloudASM?retryWrites=true&w=majority";
+
+app.use(express.json());
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(r => app.listen(process.env.PORT, '0.0.0.0'))// app.listen(process.env.port))
+    .catch(e => console.log(e));
+
 app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.use('/categories', require('./routes/categoryRoutes'));
-app.use('/products', require('./routes/productRoutes'));
+// Routes
+const categoryRoutes = require('./routes/categoryRoutes');
+const productRoutes = require('./routes/productRoutes');
 
-// Start the server
-app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
-});
+// Serve static files from the "public" directory
+app.use(express.static('public'));
+
+app.use('/categories', categoryRoutes);
+app.use('/products', productRoutes);
