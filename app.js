46747f27a76628b16d8749161fb5f7b5dc9bb539
Lyquythuan3112 <adlqt312@gmail.com>
2023-06-20 20:30:41 +0700

    Update app.js

--- app.js
+++ app.js
@@ -4,16 +4,13 @@ const mongoose = require('mongoose');
 const app = express();
 const port = process.env.PORT || 3000;
 
-MONGODB_URI='mongodb+srv://Andy:123456789aA@asmcloud.3jelcj0.mongodb.net/CloudASM?retryWrites=true&w=majority'
-
-
 // MongoDB connection
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
-.then(() => console.log('Connected to MongoDB'))
-.catch((error) => console.error('Failed to connect to MongoDB', error));
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((error) => console.error('Failed to connect to MongoDB', error));
 
 // Middleware
 app.use(express.urlencoded({ extended: true }));
@@ -21,6 +18,10 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 // Routes
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
 app.use('/categories', require('./routes/categoryRoutes'));
 app.use('/products', require('./routes/productRoutes'));
 
