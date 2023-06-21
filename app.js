d2b783926d0cba92514e315b5c171a0814025981
Lyquythuan3112 <adlqt312@gmail.com>
2023-06-21 08:40:03 +0700

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const app = express();
+require('dotenv').config();
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
