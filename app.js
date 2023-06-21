31ad59c285dc73ee9215acab70d1a463d681f25c
Lyquythuan3112 <adlqt312@gmail.com>
2023-06-21 07:42:04 +0700

    a

--- app.js
+++ app.js
@@ -1,57 +1,57 @@
-// app.js
+// // app.js
 
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const dotenv = require('dotenv'); // Import dotenv package
-const app = express();
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const bodyParser = require('body-parser');
+// const dotenv = require('dotenv'); // Import dotenv package
+// const app = express();
 
-// Load environment variables from .env file
-dotenv.config();
+// // Load environment variables from .env file
+// dotenv.config();
 
-// Middleware
-app.use(bodyParser.urlencoded({ extended: true }));
-app.set('view engine', 'ejs');
-app.set('views', 'views'); // Specify the directory for views
+// // Middleware
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.set('view engine', 'ejs');
+// app.set('views', 'views'); // Specify the directory for views
 
-// MongoDB Connection
-const mongodbUri = process.env.MONGODB_URI="mongodb+srv://Andy:123456789aA@asmcloud.3jelcj0.mongodb.net/CloudASM?retryWrites=true&w=majority";
+// // MongoDB Connection
+// const mongodbUri = process.env.MONGODB_URI="mongodb+srv://Andy:123456789aA@asmcloud.3jelcj0.mongodb.net/CloudASM?retryWrites=true&w=majority";
 
-mongoose
-  .connect(mongodbUri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log('Connected to MongoDB');
-    // Start the server after successful MongoDB connection
-    const port = process.env.PORT || 3000;
-    app.listen(port, () => {
-      console.log(`Server started on port ${port}`);
-    });
-  })
-  .catch((error) => {
-    console.error('Failed to connect to MongoDB', error);
-  });
+// mongoose
+//   .connect(mongodbUri, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => {
+//     console.log('Connected to MongoDB');
+//     // Start the server after successful MongoDB connection
+//     const port = process.env.PORT || 3000;
+//     app.listen(port, () => {
+//       console.log(`Server started on port ${port}`);
+//     });
+//   })
+//   .catch((error) => {
+//     console.error('Failed to connect to MongoDB', error);
+//   });
 
-  // Routes
-const categoryRoutes = require('./routes/categoryRoutes');
-const productRoutes = require('./routes/productRoutes');
+//   // Routes
+// const categoryRoutes = require('./routes/categoryRoutes');
+// const productRoutes = require('./routes/productRoutes');
 
-app.get('/', (req, res) => {
-  res.render('index');
-});
-app.get('/categories', (req, res) => {
-  res.render('category',categoryRoutes);
-});
-app.get('/products', (req, res) => {
-  res.render('product',productRoutes);
-});
+// app.get('/', (req, res) => {
+//   res.render('index');
+// });
+// app.get('/categories', (req, res) => {
+//   res.render('category',categoryRoutes);
+// });
+// app.get('/products', (req, res) => {
+//   res.render('product',productRoutes);
+// });
 
 
 
-// Serve static files from the "public" directory
-app.use(express.static('public'));
+// // Serve static files from the "public" directory
+// app.use(express.static('public'));
 
-// app.use('/categories', categoryRoutes);
-// app.use('/products', productRoutes);
+// // app.use('/categories', categoryRoutes);
+// // app.use('/products', productRoutes);
